3b8e4614d731ef8f99d90701b73399e0bffd176b
damienmillet <contact@damien-millet.dev>
2021-08-29 18:13:31 +0200

    ♻️ optimise environment's variable

--- lib/utils/encoder.ts
+++ lib/utils/encoder.ts
@@ -1,10 +1,25 @@
 import * as crypto from "crypto";
 type data = { algorithm: string; key: string; iv: string };
 
+const TOKEN_SECRET = process.env.TOKEN_SECRET;
+const TOKEN_IV = process.env.TOKEN_IV;
+
+if (!TOKEN_SECRET) {
+  throw new Error(
+    "Please define the TOKEN_SECRET environment variable inside .env.local"
+  );
+}
+
+if (!TOKEN_IV) {
+  throw new Error(
+    "Please define the TOKEN_IV environment variable inside .env.local"
+  );
+}
+
 const data: data = {
   algorithm: "aes-256-cbc",
-  key: `${process.env.TOKEN_SECRET}`,
-  iv: `${process.env.TOKEN_IV}`,
+  key: TOKEN_SECRET,
+  iv: TOKEN_IV,
 };
 
 export const encode = (value: string): string => {
